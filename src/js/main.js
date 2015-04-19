2deb847a55cd18a3f3088d8e9ec5af1551398a26
aintnorest <cjalatorre@gmail.com>
2015-04-18 21:53:45 -0700

    pushing this empty react program

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+//STYLES
+var sMain = require("../styles/sass/Main.scss");
+var sShared = require("../styles/sass/Shared.scss");
+sMain.use();
+sShared.use();
+//
+console.log("Working Main Javascript File");
